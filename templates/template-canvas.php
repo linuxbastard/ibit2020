e278648b1e5c3cd6a0468e85b8622f2216ffe786
Ronaldo Bartolome <bart@mister-fixit.com>
2020-02-06 14:56:57 +0800

    create headerless template
    create canvas template with no header menu and no footer

---
+++ templates/template-canvas.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: IBIT Canvas Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage ibit2020
+ * @since 1.0
+ */
+
+get_template_part( 'singular' );
